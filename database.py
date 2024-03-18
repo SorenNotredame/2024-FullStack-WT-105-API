34e75fa3ae738eac840c15d07ab004736a15efa7
SorenNotredame <r0982640@student.thomasmore.be>
2024-03-18 11:32:11 +0100

    vercel test

--- database.py
+++ database.py
@@ -1,17 +1,20 @@
 import mysql.connector
 import config
 
+
 def connect_to_database():
     try:
-        connection = mysql.connector.connect(host=config.db_hostname, user=config.db_username, password=config.db_password)
+        connection = mysql.connector.connect(host=config.db_hostname, user=config.db_username,
+                                             password=config.db_password)
         return connection
     except mysql.connector.Error as error:
         print("Error connecting to database:", error)
         return error
 
-def execute_sql_query(sql_query, query_parameters = None):
+
+def execute_sql_query(sql_query, query_parameters=None):
     connection = connect_to_database()
-    result=''
+    result = ''
     try:
         cursor = connection.cursor()
         cursor.execute(sql_query, query_parameters)
@@ -27,7 +30,6 @@ def execute_sql_query(sql_query, query_parameters = None):
 
         print("Error executing SQL query:", exception)
         result = exception
-
 
     finally:
         if connection.is_connected():
