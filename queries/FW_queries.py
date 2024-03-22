418219becfed51d462ad0fd0124f7595ff89f21a
FrankWagemans <r0447770@student.thomasmore.be>
2024-03-22 21:11:14 +0100

    get timeslots

--- queries/FW_queries.py
+++ queries/FW_queries.py
@@ -4,7 +4,7 @@ insert_dealer_query = "INSERT INTO brusselsbrews.new_dealer (businessname, addre
 
 dealerinfo_query = "SELECT dealer.dealername, dealer.streetAndNumber, dealer.postal_codeAndCity, dealer.phone, dealer.email, dealer.opening_hours, GROUP_CONCAT(product.product_name) FROM brusselsbrews.dealer JOIN brusselsbrews.dealer_product ON dealer.dealer_id = dealer_product.dealer_id JOIN brusselsbrews.product ON dealer_product.product_id = product.product_id GROUP BY dealer.dealer_id;"
 
-insert_tour_query = "INSERT INTO brusselsbrews.tour (name, email, phone, date, amount) VALUES (%s, %s, %s, %s, %s);"
+insert_tour_query = "INSERT INTO brusselsbrews.tour (name, email, phone, date, timeslot, amount) VALUES (%s, %s, %s, %s, %s, %s);"
 
 insert_tasting_query = "INSERT INTO brusselsbrews.tasting (name, address, email, phone, date, amount) VALUES (%s, %s, %s, %s, %s, %s);"
 
