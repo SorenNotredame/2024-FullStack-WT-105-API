fd6b0805252ddc12b3a8b7c3c60e5e269b71d998
FrankWagemans <r0447770@student.thomasmore.be>
2024-03-22 14:06:39 +0100

    API FW

--- models/FW_models.py
+++ models/FW_models.py
@@ -1,1 +1,25 @@
-# Author:
+# Author: Frank Wagemans
+
+from pydantic import BaseModel
+from datetime import date
+
+class new_dealer(BaseModel):
+    businessname: str
+    address: str
+    phone: str
+    email: str
+
+class tour(BaseModel):
+    name: str
+    email: str
+    phone: str
+    date: date
+    amount: int
+
+class tasting(BaseModel):
+    name: str
+    address: str
+    email: str
+    phone: str
+    date: date
+    amount: int
