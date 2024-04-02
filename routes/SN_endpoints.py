ccff6b28600839ee4460e61498a386c2d2ba9a7c
SorenNotredame <r0982640@student.thomasmore.be>
2024-04-02 18:17:34 +0200

    webshop post orders

--- routes/SN_endpoints.py
+++ routes/SN_endpoints.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter
 import database
 from queries import SN_queries as queries
+from models import SN_models as models
 
 app = APIRouter()
 
@@ -20,6 +21,7 @@ def get_dropdown_info():
         })
     return {'dropdown': info_to_return}
 
+
 @app.get('/beers')
 def get_beer_info():
     query = queries.beers_query
@@ -36,6 +38,7 @@ def get_beer_info():
             "image": beer[4]
         })
     return {'beers': beers_to_return}
+
 
 @app.get('/webshop')
 def get_webshop_beer_info():
@@ -54,3 +57,37 @@ def get_webshop_beer_info():
             "image": beer[5]
         })
     return {'beers': beers_to_return}
+
+
+@app.post("/order")
+def create_order(order: models.Orders):
+    query = queries.webshop_insert_order
+    success = database.execute_sql_query(query, (
+        order.name,
+        order.total,
+        order.date,
+    ))
+    if success:
+        return order
+
+
+@app.post("/order_beers")
+def create_order_beers(order_beers: models.OrderBeers):
+    query = queries.webshop_insert_beers_order
+    success = database.execute_sql_query(query, (
+        order_beers.ordersId,
+        order_beers.beer,
+        order_beers.bottles,
+        order_beers.crates,
+    ))
+    if success:
+        return order_beers
+
+
+@app.get("/get_order_id")
+def get_order_id(name: str):
+    query = queries.get_orders_id
+    order = database.execute_sql_query(query, (name,))
+    if isinstance(order, Exception):
+        return order, 500
+    return {"orderId": order[0][0]}
