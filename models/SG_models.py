27132a4edeb0a9075b82cf66c389e498e125b76c
Stef Gebruers <r0989521@student.thomasmore.be>
2024-04-09 09:46:50 +0200

    Contactforms endpoint created

--- models/SG_models.py
+++ models/SG_models.py
@@ -1,2 +1,14 @@
-# GEBRUERS Stef
+# Author: GEBRUERS Stef
 
+from pydantic import BaseModel
+from datetime import date
+
+
+class ContactForm(BaseModel):
+    firstName: str
+    surName: str
+    emailAddress: str
+    content: str
+    terms: int
+    addToMailingList: int
+    submitDate: date
--- queries/SG_queries.py
+++ queries/SG_queries.py
@@ -1,3 +1,5 @@
-# GEBRUERS Stef
+# Author: GEBRUERS Stef
 
-get_all_employeecards = "select employeeId, firstName, alias, firstWords, bio, linkToPicture, bioTitle from brusselsbrews.employee;"
+get_all_employeecards = "SELECT employeeId, firstName, alias, firstWords, bio, linkToPicture, bioTitle from brusselsbrews.employee;"
+
+submit_new_contactform = "INSERT INTO brusselsbrews.contactforms (firstName, surName, emailAddress, content, termsAndConditions, addToMailingList, submitDate) VALUES (%s, %s, %s, %s, %s, %s, %s);"
