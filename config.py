172cc4d4e7d861e6b7d4a65fec0d8a8e801afaac
Stef Gebruers <r0989521@student.thomasmore.be>
2024-04-10 21:14:28 +0200

    Events endpoint created

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ db_password = os.environ.get('DB_PASSWORD')
 db_hostname = os.environ.get('DB_HOSTNAME')
 cors_origins = os.environ.get("ALLOWED_ORIGINS", "*")
 documentation_url = os.environ.get('DOCS_URL', None)
-# If the env files is not uploaded, then this link to docs will only be available offline and not online
+# If the env files is not uploaded, then this link to docs will only be avaicmdlable offline and not online
